36c21a62d32ec9476c995a789bf00de8a24b69dd
TWTwoFace <pipo2017@mail.ru>
2025-06-13 01:31:29 +0300

    feat: player character run, idle, jump anims

--- Assets/Scripts/PlayerAnimationController.cs
+++ Assets/Scripts/PlayerAnimationController.cs
@@ -1,16 +1,36 @@
 using UnityEngine;
+using KinematicCharacterController;
 
 public class PlayerAnimationController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private KinematicCharacterMotor _characterMotor;
+    [SerializeField] private Animator _animator;
 
-    // Update is called once per frame
-    void Update()
+    private bool _jumped;
+    
+    private void Update()
     {
+        if (_characterMotor.GroundingStatus.IsStableOnGround && _jumped && _characterMotor.Velocity.y < 2)
+        {
+            _animator.SetBool("Jumped", true);
+            _jumped = false;
+        }
         
+        if (_characterMotor.Velocity.y > 2 && !_jumped)
+        {
+            _jumped = true;
+            _animator.SetTrigger("Jump");
+            _animator.SetBool("Jumped", false);
+            print("a");
+        }
+        
+        if (Mathf.Abs(_characterMotor.Velocity.x) > 0.4 || Mathf.Abs(_characterMotor.Velocity.z) > 0.4)
+        {
+            _animator.SetBool("IsRunning", true);
+        }
+        else
+        {
+            _animator.SetBool("IsRunning", false);
+        }
     }
 }
