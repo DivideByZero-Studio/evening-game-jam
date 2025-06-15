1d078d56f1968030637173f747bc1a060edaf39e
TWTwoFace <pipo2017@mail.ru>
2025-06-15 06:58:28 +0300

    feat: add interactable hints

--- Assets/Scripts/LevelEndInteractable.cs
+++ Assets/Scripts/LevelEndInteractable.cs
@@ -5,6 +5,7 @@ public class LevelEndInteractable : MonoBehaviour, IInteractable
 {
     [SerializeField] private string levelToLoad;
     [SerializeField] private AudioClip _audioClip;
+    [SerializeField] private HintInteractable _hintInteractable;
     
     public void Interact()
     {
@@ -17,5 +18,19 @@ public class LevelEndInteractable : MonoBehaviour, IInteractable
             SceneLoader.Instance.LoadScene(levelToLoad);
         
         AudioManager.Instance.PlaySfxOneShot(_audioClip);
+        _hintInteractable.HideWithParticles();
+        enabled = false;
+    }
+    
+    public void ShowHint()
+    {
+        if (enabled == false)
+            return;
+        _hintInteractable.Show();
+    }
+
+    public void HideHint()
+    {
+        _hintInteractable.Hide();
     }
 }
--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -3,9 +3,18 @@ using UnityEngine;
 public class Obstacle : MonoBehaviour, IInteractable
 {
     [SerializeField] private AudioClip _notEnterSound;
-    
     public void Interact()
     {
         AudioManager.Instance.PlaySfxOneShot(_notEnterSound);
+    }
+    
+    public void ShowHint()
+    {
+
+    }
+
+    public void HideHint()
+    {
+        
     }
 }
