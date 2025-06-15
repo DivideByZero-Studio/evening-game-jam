328bd6abbd1e73f9533374ea30587efe3f3f805b
TWTwoFace <pipo2017@mail.ru>
2025-06-15 04:01:24 +0300

    feat: add lever sound

--- Assets/Scripts/LevelEndInteractable.cs
+++ Assets/Scripts/LevelEndInteractable.cs
@@ -4,12 +4,18 @@ using UnityEngine.SceneManagement;
 public class LevelEndInteractable : MonoBehaviour, IInteractable
 {
     [SerializeField] private string levelToLoad;
+    [SerializeField] private AudioClip _audioClip;
     
     public void Interact()
     {
+        if (enabled == false)
+            return;
+        
         if (string.IsNullOrEmpty(levelToLoad))
             SceneLoader.Instance.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         else
             SceneLoader.Instance.LoadScene(levelToLoad);
+        
+        AudioManager.Instance.PlaySfxOneShot(_audioClip);
     }
 }
--- Assets/Scripts/Lever.cs
+++ Assets/Scripts/Lever.cs
@@ -3,16 +3,22 @@ using UnityEngine;
 public class Lever : MonoBehaviour, IInteractable
 {
     [SerializeField] private Animator _animator;
-    [SerializeField] private GameObject _levelEndInteractable;
+    [SerializeField] private LevelEndInteractable _levelEndInteractable;
     [SerializeField] private GameObject _obstacle;
-    
+    [SerializeField] private AudioClip _audioClip;
 
+    private void Start()
+    {
+        _levelEndInteractable.enabled = false;
+    }
+    
     public void Interact()
     {
         if (_obstacle == null)
             return;
         _animator.SetTrigger("Interact");
-        _levelEndInteractable.AddComponent<LevelEndInteractable>();
+        AudioManager.Instance.PlaySfxOneShot(_audioClip);
+        _levelEndInteractable.enabled = true;
         Destroy(_obstacle);
     }
 }
